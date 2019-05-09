e487e4570c1e00b1e8164f48311d0fae2bbef876
Administrator <me@me>
2019-05-09 20:09:03 +0800

    v1.0

---
+++ function.py
@@ -0,0 +1,36 @@
+houdu = {'bihousw': 10, 'bihoudw': 5, 'yaogaisw': 7, 'yaogaidw': 5, 'dianban': 1, 'neika_geban': 3}  # 厚度
+caizhi = {'sw': '180g/120g/100g/120g/160g', 'dw': 'K230g/H140g/K230g'}  # 材质
+neika_geban = {'sw': 15, 'dw': 10}
+
+
+def rounding(num):
+    if str(num)[-1] == '1':
+        return num - 1
+    elif str(num)[-1] == '9':
+        return num + 1
+    else:
+        return num
+
+
+def calculator(dsw, guanzhijing, guangao, gaigao, guan_heng_c, guan_shu_c, neika_heng_c, neika_shu_c, geban_c, dianban_c, yinshua):
+    bihou = houdu['bihou' + dsw]  # 壁厚
+    yaogai = houdu['yaogai' + dsw]  # 摇盖厚
+    L = (guanzhijing + 1) * guan_heng_c + bihou + 3 + houdu['neika_geban'] * neika_heng_c  #
+    W = (guanzhijing + 1) * guan_shu_c + bihou + 3 + houdu['neika_geban'] * (neika_shu_c + geban_c)  #
+    H = (guangao + gaigao - 7) + bihou + yaogai + houdu['dianban'] * dianban_c  #
+
+    neibaozhuang = ''
+    if neika_heng_c:
+        neika_L = rounding(L - neika_geban[dsw])
+        neika_W = rounding(W - neika_geban[dsw])
+        neika_H = rounding(H - neika_geban[dsw])
+        neibaozhuang += '内卡：{}x{}x{}mm'.format(neika_L, neika_W, neika_H)
+    if geban_c:
+        geban_L = rounding(L - neika_geban[dsw])
+        geban_H = rounding(H - neika_geban[dsw])
+        neibaozhuang += '隔{}：{}x{}mm'.format(geban_c, geban_L, geban_H)
+    if dianban_c:
+        dianban_L = rounding(L - neika_geban[dsw])
+        dianban_W = rounding(W - neika_geban[dsw])
+        neibaozhuang += '垫板：{}x{}mm'.format(dianban_L, dianban_W)
+    return "{}\n{}*{}*{}mm\n{}\n{}".format(caizhi[dsw], L, W, H, yinshua, neibaozhuang)
---
+++ main.py
@@ -0,0 +1,87 @@
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+import gui
+from function import *
+
+guanzhijing, guangao, gaigao = 65, 240, 55  # 罐直径
+dsw = 'sw'  # 单瓦或双瓦
+guan_heng_c, guan_shu_c = 6, 4  # 横排罐数，竖排罐数
+neika_heng_c, neika_shu_c, geban_c, dianban_c = 5, 3, 0, 1
+yinshua = '水墨印'
+
+
+class MyWindow(gui.Ui_MainWindow):
+    def __init__(self,Dialog):
+        super().setupUi(Dialog)
+        self.pushButton.clicked.connect(self.calculate)
+        self.buttonGroup.buttonClicked.connect(self.rbclicked)
+        self.buttonGroup_2.buttonClicked.connect(self.rbclicked)
+        self.buttonGroup_3.buttonClicked.connect(self.rbclicked)
+        self.buttonGroup_4.buttonClicked.connect(self.rbclicked)
+        self.buttonGroup_5.buttonClicked.connect(self.rbclicked)
+        self.buttonGroup_6.buttonClicked.connect(self.rbclicked)
+
+    def calculate(self):
+        global guangao, gaigao
+        guangao = int(self.lineEdit_4.text())
+        gaigao = int(self.lineEdit_5.text())
+        text = calculator(dsw, guanzhijing, guangao, gaigao, guan_heng_c, guan_shu_c, neika_heng_c, neika_shu_c, geban_c, dianban_c, yinshua)
+        self.textBrowser.setText(text)
+
+    def rbclicked(self):
+        global guanzhijing, dsw, guan_heng_c, guan_shu_c, neika_heng_c, neika_shu_c, geban_c, dianban_c, yinshua
+        if self.buttonGroup.checkedButton() == self.radioButton_12:
+            guanzhijing = 65
+        elif self.buttonGroup.checkedButton() == self.radioButton_13:
+            guanzhijing = 60
+        elif self.buttonGroup.checkedButton() == self.radioButton_15:
+            guanzhijing = 52
+        else:
+            guanzhijing = int(self.lineEdit_3.text())
+
+        if self.buttonGroup_2.checkedButton() == self.radioButton:
+            dsw = 'sw'
+        else:
+            dsw = 'dw'
+
+        if self.buttonGroup_3.checkedButton() == self.radioButton_3:
+            guan_heng_c, guan_shu_c = 6, 4
+        elif self.buttonGroup_3.checkedButton() == self.radioButton_4:
+            guan_heng_c, guan_shu_c = 4, 3
+        else:
+            guan_heng_c, guan_shu_c = int(self.lineEdit.text()), int(self.lineEdit_2.text())
+
+        if self.buttonGroup_4.checkedButton() == self.radioButton_6:
+            neika_heng_c, neika_shu_c = guan_heng_c-1, guan_shu_c-1
+            geban_c = 0
+        elif self.buttonGroup_4.checkedButton() == self.radioButton_7:
+            geban_c = guan_shu_c-1
+            neika_heng_c, neika_shu_c = 0, 0
+        else:
+            neika_heng_c, neika_shu_c, geban_c = 0, 0, 0
+
+        if self.buttonGroup_5.checkedButton() == self.radioButton_9:
+            dianban_c = 1
+        elif self.buttonGroup_5.checkedButton() == self.radioButton_10:
+            dianban_c = 2
+        else:
+            dianban_c = 0
+
+        if self.buttonGroup_6.checkedButton() == self.radioButton_16:
+            yinshua = '水墨印'
+        elif self.buttonGroup_6.checkedButton() == self.radioButton_17:
+            yinshua = '水印'
+        else:
+            yinshua = self.lineEdit_6.text()
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = MyWindow(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
